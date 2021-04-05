61bd050131f0d4c37df9ab0040b18f54645ced93
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2021-04-05 12:49:51 +0200

    Create Hack-fb.py

---
+++ Hack-fb.py
@@ -0,0 +1,37 @@
+import os
+import time
+import random
+os.system("clear")
+print ("\033[0;101m|» DEV BY : LWES ELTASA HACK |")
+print ("\033[0;105m|» instagram : _0abdoo0_ |")
+print("\033[0;104m|» whatsapp : 01283494514 |")
+print ("\033[0;101")
+print ("\033[0;31m                   ,.")
+print ("\033[0;32m                 ,   `.")
+print ("\033[0;33m               ,  _<>_ `.")
+print ("\033[0;34m             , .- ____`-.`.")
+print ("\033[0;35m           , _.-      ``-._`.")
+print ("\033[0;36m         , ,       /\      `.`.")
+print ("\033[0;37m       ,  /.._  O /  \ O  _.,\ `.")
+print ("\033[0;38m     , / /  \ ``-;.--.:-   /  \ \`.")
+print ("\033[0;39m   ,  : :    \  /\`., /\  /    : : `.")
+print ("\033[0;31m  < <>| |    LWES ELTASA hack :-.;.:.›")
+print ("\033[0;32m   `. : :    /  \/, `.\/  \    ; ; ,") 
+print ("\033[0;33m     `.\ \  /_..-:`-- ;-.._\  / /") 
+print ("\033[0;34m       `. \`    O \  / O   ` / ,") 
+print ("\033[0;35m         `.`._     \/     _, ,") 
+print ("\033[0;36m           `..``-.____.-  ,,") 
+print ("\033[0;37m             `.`-.____.- ,") 
+print ("\033[0;38m               `.  <>  ,") 
+print ("\033[0;39m                 `.  ,")  
+print ("\033[0;101")
+time.sleep(2)
+data = 89186181
+m = ("user name & password ")
+n = 0
+while data < 99999999 :
+	data = data+18
+	n = n+1
+	print ("\033[0;101m",m ,n , "›››" , data)
+	print ("\033[0;101")
+	time.sleep (3)
